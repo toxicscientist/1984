fde546c1cdaaed4038609560c42cce525c918c7d
Nerdopedia <38317316+toxicscientist@users.noreply.github.com>
2022-10-09 23:21:06 +0300

    Final goal left

--- index.js
+++ index.js
@@ -2,16 +2,18 @@
 //Multiple word censor [X]
 //Non-looping censors [X]
 //Slicing Non-looping finished censors [X]
-//Not censoring starts and/or ends of words
+//Not censoring starts [X] and/or ends of words [X]
 //Censoring full word if part thereof is matched
-function censor(string, match, options = { grawlixes: "*", loop: true, slice: true }) {
+function censor(string, match, options = { grawlixes: "*", loop: true, slice: true, start: 0, end: 0 }) {
     options.loop = options.loop || true;
     options.slice = options.slice || true;
+    options.start = options.start || 0;
+    options.end = options.end || 0;
     if (typeof match == "string")
         match = [match];
     for (let i = 0; i < match.length; i++) {
-        string = string.replace(match[i], (options.loop ? options.grawlixes.repeat(match[i].length).slice(0, (options.slice ? match[i].length : undefined)) : options.grawlixes));
+        string = string.replace(match[i], (options.loop ? options.grawlixes.repeat(match[i].length).slice(0 + (options.start || 0), (options.slice ? match[i].length + options.end : undefined || options.end)) : options.grawlixes));
     }
     return string;
 }
-console.log(censor("I hate you so much you ducking batch", ["duck", "batch"], { grawlixes: "AAAAAAAA" }));
+console.log(censor("I hate you so much you ducking batch", ["duck", "batch"], { grawlixes: "ABCDEFGHIJ", start: 2, end: 1 }));
