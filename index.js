2c483c71c61c46da160ab51442002e7473f15965
Nerdopedia <38317316+toxicscientist@users.noreply.github.com>
2022-10-09 23:06:24 +0300

    3 goals down, 2 to go

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 "use strict";
-//Multiple word censor
-//Non-looping censors
-//Slicing Non-looping finished censors
+//Multiple word censor [X]
+//Non-looping censors [X]
+//Slicing Non-looping finished censors [X]
+//Not censoring starts and/or ends of words
 //Censoring full word if part thereof is matched
-function censor(string, match, options = { grawlixes: "*" }) {
-    var newStr = string.replace(match, options.grawlixes.repeat(match.length));
-    return newStr;
+function censor(string, match, options = { grawlixes: "*", loop: true, slice: true }) {
+    options.loop = options.loop || true;
+    options.slice = options.slice || true;
+    if (typeof match == "string")
+        match = [match];
+    for (let i = 0; i < match.length; i++) {
+        string = string.replace(match[i], (options.loop ? options.grawlixes.repeat(match[i].length).slice(0, (options.slice ? match[i].length : undefined)) : options.grawlixes));
+    }
+    return string;
 }
-// console.log(censor("I hate the game amongus so much", "amongus"))
+console.log(censor("I hate you so much you ducking batch", ["duck", "batch"], { grawlixes: "AAAAAAAA" }));
