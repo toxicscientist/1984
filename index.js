6ff9f42ebe645e305a8de2bb6c1b41caecbd50a5
Nerdopedia <38317316+toxicscientist@users.noreply.github.com>
2022-10-10 11:30:20 +0300

    fully functional, cant publish

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 "use strict";
-//Multiple word censor [X]
-//Non-looping censors [X]
-//Slicing Non-looping finished censors [X]
-//Not censoring starts [X] and/or ends of words [X]
-//Censoring full word if part thereof is matched
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.censor = void 0;
 function censor(string, match, options = { grawlixes: "*", loop: true, slice: true, start: 0, end: 0 }) {
-    options.loop = options.loop || true;
-    options.slice = options.slice || true;
-    options.start = options.start || 0;
-    options.end = options.end || 0;
+    options.grawlixes = options.grawlixes ?? "*";
+    options.loop = options.loop ?? true;
+    options.slice = options.slice ?? true;
+    options.start = options.start ?? 0;
+    options.end = options.end ?? 0;
     if (typeof match == "string")
         match = [match];
     for (let i = 0; i < match.length; i++) {
-        string = string.replace(match[i], (options.loop ? options.grawlixes.repeat(match[i].length).slice(0 + (options.start || 0), (options.slice ? match[i].length + options.end : undefined || options.end)) : options.grawlixes));
+        string = string.replace(match[i], match[i].slice(0, options.start) +
+            (options.loop
+                ? options.grawlixes.repeat(match[i].length - (options.start + options.end)).slice(0, options.slice ? match[i].length : undefined)
+                : options.grawlixes.slice(0, options.slice ? match[i].length : undefined))
+            + match[i].slice(match[i].length - options.end));
     }
     return string;
 }
-console.log(censor("I hate you so much you ducking batch", ["duck", "batch"], { grawlixes: "ABCDEFGHIJ", start: 2, end: 1 }));
+exports.censor = censor;
+// console.log(censor("I hate you so much you forking batch", ["fork", "batch"], {grawlixes: "*"}))
--- index.ts
+++ index.ts
@@ -1,27 +1,31 @@
-//Multiple word censor [X]
-//Non-looping censors [X]
-//Slicing Non-looping finished censors [X]
-//Not censoring starts [X] and/or ends of words [X]
-//Censoring full word if part thereof is matched
-
 interface CensorOptions {
-    grawlixes: string;
-    loop?: boolean;
-    slice?: boolean;
-    start?: number;
-    end?: number;
+    grawlixes?: string; // What string to use to censor
+    loop?: boolean; // Whether the grawlixes should be allowed to loop if the censored word is too long
+    slice?: boolean; // Whether the grawlixes should be cut off if the censored word is too short
+    start?: number; // How many letters shouldnt be censored at the start of the word
+    end?: number;// How many letters shouldnt be censored at the end of a word
 }
 
-function censor(string:string, match:string|string[], options:CensorOptions = {grawlixes: "*", loop: true, slice: true, start: 0, end: 0}): string{
-    options.loop = options.loop || true;
-    options.slice = options.slice || true;
-    options.start = options.start || 0;
-    options.end = options.end || 0;
+export function censor(string:string, match:string|string[], options:CensorOptions = {grawlixes: "*", loop: true, slice: true, start: 0, end: 0}): string{
+    options.grawlixes = options.grawlixes ?? "*";
+    options.loop = options.loop ?? true;
+    options.slice = options.slice ?? true;
+    options.start = options.start ?? 0;
+    options.end = options.end ?? 0;
     if(typeof match == "string") match = [match]
     for (let i = 0; i < match.length; i++) {
-        string = string.replace(match[i], (options.loop ? options.grawlixes.repeat(match[i].length).slice(0 + (options.start || 0), (options.slice ? match[i].length + options.end : undefined || options.end)) : options.grawlixes));
+        string = string.replace(
+            match[i],
+            match[i].slice(0, options.start) +
+                (
+                    options.loop
+                    ? options.grawlixes.repeat(match[i].length - (options.start + options.end)).slice(0, options.slice ? match[i].length : undefined)
+                    : options.grawlixes.slice(0, options.slice ? match[i].length : undefined)
+                )
+            + match[i].slice(match[i].length - options.end)
+        );
     }
     return string
 }
 
-console.log(censor("I hate you so much you ducking batch", ["duck", "batch"], {grawlixes: "ABCDEFGHIJ", start: 2, end: 1}))
+// console.log(censor("I hate you so much you forking batch", ["fork", "batch"], {grawlixes: "*"}))
